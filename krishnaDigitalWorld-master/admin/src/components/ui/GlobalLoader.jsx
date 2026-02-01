91020f973b2b6ffe496ad0a13e5d4da88465d0f9
MOHAMMED FARVES <142089394+mohammedfarves@users.noreply.github.com>
2026-02-01 13:47:59 +0530

    Delete krishnaDigitalWorld-master directory

--- krishnaDigitalWorld-master/admin/src/components/ui/GlobalLoader.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useLoader } from '@/contexts/LoaderContext';
-
-export const GlobalLoader = () => {
-    // Global loader disabled in favor of skeleton loaders
-    return null;
-};
--- krishnaDigitalWorld-master/frontend/src/config/baseUrl.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl = `/api`;
-
-// export const baseUrl = "https://ks-fowd.onrender.com/api";
